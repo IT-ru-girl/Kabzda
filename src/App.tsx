67c5abab424ba84732a869b095e390a2b8c96107
Ksenia <ksu606@ya.ru>
2022-11-09 11:27:07 +0300

    4

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,49 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState} from 'react';
+
 import './App.css';
+import Accordion from './components/Accordion/Accordion';
+import {Rating, RatingValueType} from './components/Rating/Rating';
+import OnOff from './components/OnOff/OnOff';
+import SelfControlledAccordion from './components/SelfControlledAccordion/SelfControlledAccordion';
+import {UncontrolledRating} from './components/UncontrollesRating/UncontrolledRating';
+import UncontrolledOnOff from './components/UncontrolledOnOff/UncontrolledOnOff';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    console.log('App rendering')
+
+    let [ratingValue, setRatingValue] = useState<RatingValueType>(3)
+
+    let [accordionCollapsed, setAccordionCollapsed] = useState<boolean>(false)
+
+    let [switchOn, setSwitchOn] = useState(false)
+    // const onClickButtonHandler =()=>{
+    //     setAccordionCollapsed(accordionCollapsed)
+    // }
+
+    return (
+        <div className={'App'}>
+            <UncontrolledOnOff onChange={setSwitchOn}/> {switchOn.toString()}
+            <SelfControlledAccordion titleValue={'Menu'}/>
+            <PageTitle title={'my friends'}/>
+            <UncontrolledRating/>
+            <Rating value={ratingValue} onClick={setRatingValue}/>
+            <Accordion titleValue={'Me'} accordionCollapsed={accordionCollapsed}
+                       setAccordionCollapsed={setAccordionCollapsed}/>
+            <OnOff on={switchOn} onChange={(on) => {
+                setSwitchOn(on)
+            }}/>
+        </div>
+    );
 }
+
+type PageTitlePropsType = {
+    title: string
+}
+
+function PageTitle(props: PageTitlePropsType) {
+    console.log('AppTitle rendering')
+    return <h1>{props.title}</h1>
+}
+
 
 export default App;
---
+++ src/components/Accordion/Accordion.tsx
@@ -0,0 +1,50 @@
+import React from 'react';
+
+type AccordionPropsType = {
+    titleValue: string
+    accordionCollapsed: boolean
+    setAccordionCollapsed: (accordionCollapsed: boolean) => void
+}
+
+
+function Accordion(props: AccordionPropsType) {
+    console.log('SelfControlledAccordion rendered')
+    return (
+        <div>
+            <AccordionTitle accordionCollapsed={props.accordionCollapsed}
+                            setAccordionCollapsed={props.setAccordionCollapsed} title={props.titleValue}/>
+            {!props.accordionCollapsed && <AccordionBody/>}
+        </div>
+    );
+}
+
+type AccordionTitleType = {
+    title: string
+    setAccordionCollapsed: (accordionCollapsed: boolean) => void
+    accordionCollapsed: boolean
+}
+
+function AccordionTitle(props: AccordionTitleType) {
+    console.log('AccordionTitle rendered')
+    return (
+        <div>
+            <h3 onClick={() => {
+                props.setAccordionCollapsed(!props.accordionCollapsed)
+            }}> {props.title}</h3>
+        </div>
+    );
+}
+
+function AccordionBody() {
+    return (
+        <div>
+            <ul>
+                <li>1</li>
+                <li>2</li>
+                <li>3</li>
+            </ul>
+        </div>
+    );
+}
+
+export default Accordion;
